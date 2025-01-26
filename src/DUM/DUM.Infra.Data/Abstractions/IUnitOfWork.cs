48165e45f40275dc9dde1aae5a225c369f55bc29
Reinaldo Sales <httpsreinaldo@gmail.com>
2025-01-25 21:45:11 -0300

    add unitofwork base

---
+++ src/DUM/DUM.Infra.Data/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace DUM.Infra.Data.Abstractions;
+
+public interface IUnitOfWork
+{
+    IDisposable Session { get; }
+    
+    void AddOperation(Action operation);
+    
+    void CleanOperations();
+
+    Task CommitChanges();
+}
---
+++ src/DUM/DUM.Infra.Data/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using DUM.Infra.Data.Abstractions;
+using MongoDB.Driver;
+
+namespace DUM.Infra.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private IClientSessionHandle session { get; }
+    public IDisposable Session => this.session;
+    
+    private List<Action> _operations { get; set; }
+
+    public UnitOfWork()
+    {
+        var mongoClient = new MongoClient("connectionString");
+        this.session = mongoClient.StartSession();
+
+        this._operations = new List<Action>();
+    }
+    
+    public void AddOperation(Action operation)
+    {
+        this._operations.Add(operation);
+    }
+
+    public void CleanOperations()
+    {
+        this._operations.Clear();
+    }
+
+    public async Task CommitChanges()
+    {
+        this.session.StartTransaction();
+
+        this._operations.ForEach(o =>
+        {
+            o.Invoke();
+        });
+
+        await this.session.CommitTransactionAsync();
+
+        this.CleanOperations();
+    }
+}
